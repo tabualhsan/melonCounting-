ae2215db1d5cd48ad8dbbd7bed57a7576e3eb5f5
vagrant <vagrant@vagrant.vm>
2021-01-29 03:49:39 +0000

    finished

---
+++ melonCounting.js
@@ -0,0 +1,36 @@
+
+
+"use strict"
+
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+
+function countMelons(melonArray){
+   
+
+    const melonCounts = {};
+
+    for(const melon of melonArray){
+     if(melonCounts[melon]) {
+        melonCounts[melon] = melonCounts[melon] + 1;
+   
+    
+        }else{
+            melonCounts[melon] = 1;
+    }
+    }
+    return melonCounts
+
+}
+
+console.log(countMelons('christmas melon', 'crenshaw', 'watermelon'))
